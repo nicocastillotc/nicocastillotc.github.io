37b017a356f764f1af61b248be56e427f0ccf586
nicocastillotc <126688301+nicocastillotc@users.noreply.github.com>
2023-03-30 21:31:11 -0300

    initial commit

---
+++ java script/eventos.js
@@ -0,0 +1,59 @@
+function gris1() {
+Element =document.getElementById("pers");
+Element.src="img/personal2.png";
+}
+function negro1() {
+    Element =document.getElementById("pers");
+    Element.src="img/personal.png";
+}
+
+function gris2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral2.png";
+}
+ function negro2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral.png";
+}
+
+function gris3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades2.png";
+}
+function negro3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades.png";
+}
+
+function gris4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios2.png";
+}
+function negro4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios.png";
+}
+
+function crudo1() {
+    Element =document.getElementById("be");
+    Element.src="img/Fb.png";
+}
+function cocido1() {
+    Element =document.getElementById("be");
+    Element.src="img/fb2.png";
+}
+
+
+function crudo2() {
+    Element =document.getElementById("li");
+    Element.src="img/ws.png";
+}
+function cocido2() {
+    Element =document.getElementById("li");
+    Element.src="img/ws2.png";
+
+}
+function ARRIBA() {
+    Element =document.getElementById("ARRIBA");
+    Element.src="ARRIBA.png";
+}
